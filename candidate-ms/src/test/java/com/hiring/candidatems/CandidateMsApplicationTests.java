b978f1cea6db2995683119100327fd5f026a0aa4
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-05-05 21:18:09 +0530

    INIT: candidate microservice

---
+++ candidate-ms/src/test/java/com/hiring/candidatems/CandidateMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiring.candidatems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
