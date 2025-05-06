90af0231bd74d0d21f3136e0a0ab6db532863c53
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-05-06 13:25:36 +0530

    INIT: status microservice

---
+++ status-ms/src/main/java/com/hiring/statusms/StatusMsApplication.java
@@ -0,0 +1,13 @@
+package com.hiring.statusms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusMsApplication.class, args);
+    }
+
+}
---
+++ status-ms/src/test/java/com/hiring/statusms/StatusMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiring.statusms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
