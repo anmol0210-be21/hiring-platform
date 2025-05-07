3cf12df56a4153bd337d3f97ccf7f965e3f0765c
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-05-07 22:12:56 +0530

    FEAT: move db to mysql

--- status-ms/src/main/java/com/hiring/statusms/domain/entity/Status.java
+++ status-ms/src/main/java/com/hiring/statusms/domain/entity/Status.java
@@ -1,23 +1,21 @@
 package com.hiring.statusms.domain.entity;
 
 import com.hiring.statusms.domain.enums.StatusType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
+@Entity
 @Data
-@Document(collection = "status")
 public class Status {
     @Id
+    @GeneratedValue(generator = "uuid")
     private UUID id;
 
     private UUID candidateId;
 
     private StatusType statusType;
-
-    public Status() {
-        this.id = UUID.randomUUID();
-    }
 }
--- status-ms/src/main/java/com/hiring/statusms/repository/StatusRepository.java
+++ status-ms/src/main/java/com/hiring/statusms/repository/StatusRepository.java
@@ -1,14 +1,14 @@
 package com.hiring.statusms.repository;
 
 import com.hiring.statusms.domain.entity.Status;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface StatusRepository extends MongoRepository<Status, UUID> {
+public interface StatusRepository extends JpaRepository<Status, UUID> {
     Optional<Status> findByCandidateId(UUID candidateId);
 
     boolean existsByCandidateId(UUID candidateId);
