fb971df36bf860be2c8f8adf1d9347869859ac95
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-05-07 10:18:21 +0530

    INIT: document microservice

---
+++ document-ms/src/test/java/com/hiring/documentms/DocumentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiring.documentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
